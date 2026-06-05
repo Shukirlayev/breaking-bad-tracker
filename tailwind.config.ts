aeeb7b909cddbb4efb72b40c9bcd07f074517a92
Sarvar Shukirlayev <Shukirlayevs@gmail.com>
2026-06-05 09:13:29 +0500

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,11 +2,22 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: "#050505",
+        surface: "#121212",
+        primaryText: "#f5f5f7",
+        secondaryText: "#86868b",
+        bbGreen: "#18c744",
+        bbDarkGreen: "#0F5B30"
+      },
+    },
   },
   plugins: [],
 };
+
 export default config;
